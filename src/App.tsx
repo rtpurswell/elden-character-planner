6164b9645a4de1825c8c52a8296f407d9fd9f0ed
Robert Purswell <taylor@tpurswell.com>
2023-01-22 18:13:24 -0700

    Moved general app structure into layour component. Added redux store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { useState } from 'react'
-import H1 from './components/common/H1'
-import Modal from './components/common/Modal'
-import Equipment from './components/Equipment'
-import Stats from './components/Stats'
-import WeaponViewer from './components/WeaponViewer'
+import Layout from './components/Layout'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+
+const store = configureStore
 function App() {
-  const [showModal, setShowModal] = useState(false)
-
-  const handleModalClose = () => {
-    setShowModal(false)
-  }
-  const handleModalOpen = () => {
-    setShowModal(true)
-  }
   return (
-    <main className="w-full flex flex-col md:grid md:grid-cols-2  text-white gap-10 p-5">
-      <div className="col-span-2">
-        <H1>Elden Ring Character Planner</H1>{' '}
-      </div>
-      <div className=" bg-slate-800 flex flex-col p-5 rounded">
-        <Equipment />
-        <button className="p-3 bg-green-600" onClick={handleModalOpen}>
-          Show it!
-        </button>
-        {showModal ? (
-          <Modal title="Select Weapon" onClose={handleModalClose}>
-            <WeaponViewer />
-          </Modal>
-        ) : null}
-      </div>
-
-      <div className=" bg-slate-800 p-5 rounded">
-        <Stats />
-      </div>
-    </main>
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   )
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import ClassSelector from './ClassSelector'
+import H1 from './common/H1'
+import Modal from './common/Modal'
+import Equipment from './Equipment'
+import Stats from './Stats'
+import WeaponViewer from './WeaponViewer'
+
+function Layout() {
+  const [showModal, setShowModal] = useState(false)
+
+  const handleModalClose = () => {
+    setShowModal(false)
+  }
+  const handleModalOpen = () => {
+    setShowModal(true)
+  }
+
+  return (
+    <main className="w-full flex flex-col md:grid md:grid-cols-2  text-white gap-10 p-5">
+      <div className="md:col-span-2">
+        <H1>Elden Ring Character Planner</H1>{' '}
+      </div>
+      <div className="md:col-span-2">
+        <ClassSelector />
+      </div>
+      <div className=" bg-slate-800 flex flex-col p-5 rounded">
+        <Equipment />
+        <button className="p-3 bg-green-600" onClick={handleModalOpen}>
+          Show it!
+        </button>
+        {showModal ? (
+          <Modal title="Select Weapon" onClose={handleModalClose}>
+            <WeaponViewer />
+          </Modal>
+        ) : null}
+      </div>
+
+      <div className=" bg-slate-800 p-5 rounded">
+        <Stats />
+      </div>
+    </main>
+  )
+}
+
+export default Layout
