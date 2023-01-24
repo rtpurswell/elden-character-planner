53af657bc08177069d99defa2aba4a99fa59595c
Robert Purswell <taylor@tpurswell.com>
2023-01-24 12:52:58 -0700

    Added placeholder components for Ashes and Incantations

---
+++ src/components/AshesOfWar.tsx
@@ -0,0 +1,11 @@
+import H2 from './common/H2'
+
+function AshesOfWar() {
+  return (
+    <div>
+      <H2>Ashes of War</H2>
+    </div>
+  )
+}
+
+export default AshesOfWar
---
+++ src/components/IncantationsAndSorceries.tsx
@@ -0,0 +1,11 @@
+import H2 from './common/H2'
+
+function IncantationsAndSorceries() {
+  return (
+    <div>
+      <H2>Ashes of War</H2>
+    </div>
+  )
+}
+
+export default IncantationsAndSorceries
