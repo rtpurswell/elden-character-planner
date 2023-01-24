020bb8407ca3025c3f36f80f7682f784e8b1eefb
Robert Purswell <taylor@tpurswell.com>
2023-01-24 12:54:14 -0700

    Updated H2 to add custom classes

--- src/components/common/H2.tsx
+++ src/components/common/H2.tsx
@@ -1,8 +1,9 @@
 interface H2Props {
   children: React.ReactNode
+  className?: string
 }
-function H2({ children }: H2Props) {
-  return <h2 className="text-2xl">{children}</h2>
+function H2({ children, className }: H2Props) {
+  return <h2 className={`text-2xl ${className}`}>{children}</h2>
 }
 
 export default H2
