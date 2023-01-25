f54a47e25bbb3cb6fdf484881d43cde467bd958b
Robert Purswell <taylor@tpurswell.com>
2023-01-25 12:12:30 -0700

    started adjusting Equipment to have slider options to edit Ashes of War

--- src/components/EquipContainer.tsx
+++ src/components/EquipContainer.tsx
@@ -1,8 +1,15 @@
+import { Children } from 'react'
 interface EquipContainerProps {
   children: React.ReactNode
 }
 const EquipContainer = ({ children }: EquipContainerProps) => {
-  return <div className="grid grid-cols-5 gap-1">{children}</div>
+  const ColSpan = 5 - Children.count(children)
+  //col-span-2 col-span-1
+  return (
+    <div className="grid grid-cols-5 gap-1">
+      {children} <div className={`bg-red-600 col-span-${ColSpan}`}></div>
+    </div>
+  )
 }
 
 export default EquipContainer
