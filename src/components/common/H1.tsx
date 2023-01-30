020ef57704b389f608a2d02cd67ca553b057201d
Robert Purswell <taylor@tpurswell.com>
2023-01-30 14:56:16 -0700

    updated H1 to use className prop

--- src/components/common/H1.tsx
+++ src/components/common/H1.tsx
@@ -1,9 +1,13 @@
 interface H1Props {
   children: React.ReactNode
-  className: string
+  className?: string
 }
 function H1({ children, className }: H1Props) {
-  return <h1 className={`text-xl md:text-2xl ${className}`}>{children}</h1>
+  return (
+    <h1 className={`text-xl md:text-2xl ${className ? className : ''}`}>
+      {children}
+    </h1>
+  )
 }
 
 export default H1
